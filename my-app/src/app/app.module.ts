57e6594b1a3a37a626a790c3e733d6a5b4a21a82
syyymq0602 <295217270@qq.com>
2021-01-24 21:29:07 +0800

    了解根模块以及组件

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ my-app/src/app/components/news/news.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
+})
+export class NewsComponent implements OnInit {
+
+  title = "你好吗"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
